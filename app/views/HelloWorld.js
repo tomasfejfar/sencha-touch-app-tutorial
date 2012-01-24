e1d4273a4c1a3812756309d15934b3192d6d6e92
Tomáš Fejfar <tomas.fejfar@gmail.com>
2012-01-24 16:21:55 +0100

    03: docked items

--- app/views/HelloWorld.js
+++ app/views/HelloWorld.js
@@ -1,6 +1,22 @@
 app.views.HelloWorld = Ext.extend(Ext.Panel, {
     initComponent : function() {
         Ext.apply(this, {
+            dockedItems : [ {
+                xtype : 'toolbar',
+                dock : 'top',
+                title : 'Hello World',
+                items : [ {
+                    xtype : 'button',
+                    text : 'Yay!'
+                } ]
+            }, {
+                xtype : 'toolbar',
+                dock : 'bottom',
+                items : [ {
+                    xtype : 'button',
+                    text : 'Nah...'
+                } ]
+            } ],
             items : [ {
                 html : 'Hello World!'
             } ]
